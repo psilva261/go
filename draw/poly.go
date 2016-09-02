b829bafe1d6f02f66f5597c1e4e9b59788f3226b
Kyle Nusbaum <KyleJNusbaum@gmail.com>
2016-09-01 19:04:08 -0500

    reworking dopoly

--- draw/poly.go
+++ draw/poly.go
@@ -19,27 +19,46 @@ func dopoly(cmd byte, dst *Image, pp []image.Point, end0, end1, radius int, src 
 		return
 	}
 
+	t := make([]byte, (len(pp) + 1)*2*3)
+	u := 0
+	ox := 0
+	oy := 0
+	for i := range pp {
+		u += addcoord(t[u:], ox, pp[i].X)
+		ox = pp[i].X
+		u += addcoord(t[u:], oy, pp[i].Y)
+		oy = pp[i].Y
+	}
+
+	// If the polygon isn't already closed, close it
+	extra := false
+	if pp[0] != pp[len(pp)-1] {
+		u += addcoord(t[u:], ox, pp[0].X)
+		ox = pp[0].X
+		u += addcoord(t[u:], oy, pp[0].Y)
+		oy = pp[0].Y
+		extra = true
+	}
+
 	setdrawop(dst.Display, op)
-	m := 1 + 4 + 2 + 4 + 4 + 4 + 4 + 2*4 + len(pp)*2*3 // too much
-	a := dst.Display.bufimage(m)                       // too much
+
+	a := dst.Display.bufimage(1+4+2+4+4+4+4+2*4+u)
+
 	a[0] = cmd
 	bplong(a[1:], uint32(dst.id))
-	bpshort(a[5:], uint16(len(pp)-1))
+	if extra {
+		// Add 1 for the extra "closing" edge.
+		bpshort(a[5:], uint16(len(pp)))
+	} else {
+		bpshort(a[5:], uint16(len(pp)-1))
+	}
 	bplong(a[7:], uint32(end0))
 	bplong(a[11:], uint32(end1))
 	bplong(a[15:], uint32(radius))
 	bplong(a[19:], uint32(src.id))
 	bplong(a[23:], uint32(sp.X))
 	bplong(a[27:], uint32(sp.Y))
-	o := 31
-	ox, oy := 0, 0
-	for _, p := range pp {
-		o += addcoord(a[o:], ox, p.X)
-		o = addcoord(a[o:], oy, p.Y)
-		ox, oy = p.X, p.Y
-	}
-	d := dst.Display
-	d.buf = d.buf[:len(d.buf)-m+o]
+	copy(a[31:], t[:u])
 }
 
 // Poly draws the open polygon p in the specified source color, with ends as
